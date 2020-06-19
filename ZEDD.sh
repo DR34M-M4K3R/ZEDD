858b56d5c1c3f597191c94de5ba031864fed762c
mZzgamer <66206932+mZzgamer@users.noreply.github.com>
2020-06-19 08:31:24 +0000

    Update ZEDD.sh

--- ZEDD.sh
+++ ZEDD.sh
@@ -102,6 +102,10 @@ if [ "$x" == "$optionu" ]; then   #OPTION UPDATE
     rm -rf ./ZEDD.SAV   
     read -p "PRESS ENTER TO CONTINUE"
     clear
+    git clone https://github.com/mZzgamer/ZEDD
+    cd ZEDD/
+    chmod +x ZEDD.sh
+    sudo bash ZEDD.sh
 fi
 
 if [ "$x" == "$optiona" ]; then   #OPTION ABOUT
